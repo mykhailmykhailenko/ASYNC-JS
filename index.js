7fa70be934a45af2cfd0b6c2ffd28eca6a191c0c
Mykhailenko Mykhailo <mikhailenkomikhail1995@gmail.com>
2022-11-02 20:05:12 +0200

    Then and Catch

--- index.js
+++ index.js
@@ -5,11 +5,28 @@ Resolve - вирішений (успішний)
 Reject - відхилений (неуспішний)
 */
 
-const resultOfRequest = fetch('https://');
+const resultOfRequest = fetch('./data.json');
 console.log(resultOfRequest);
 
-resultOfRequest.then((responce)=>{
-    console.log('promise ok')
-}, (reject)=>{
+// resultOfRequest.then((responce)=>{
+//     console.log('promise ok');
+//     return responce.json();
+// }, (reject)=>{
+//     console.log(reject);
+// }).then((successData)=>{
+//     console.log(successData)
+// },(errorData)=>{
+//     console.log(errorData);
+// })
+
+
+resultOfRequest
+.then((responce)=>{
+    console.log('promise ok');
+    return responce.json();
+}).then((successData) => {
+    console.log(successData);
+})
+.catch((reject)=>{
     console.log(reject);
 })
