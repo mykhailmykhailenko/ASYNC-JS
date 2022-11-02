563d0686f7f9cfba6707c7fc6b1a65ccc37c6fb5
Mykhailenko Mykhailo <mikhailenkomikhail1995@gmail.com>
2022-11-02 16:46:50 +0200

    JSON

--- index.js
+++ index.js
@@ -1,16 +1,24 @@
-const h1 = document.querySelector('h1');
-
-let id = setTimeout(tick, 1000);
-let counter = 0;
-
-function tick(){
-    h1.textContent = ++counter;
-    id = setTimeout(tick, 1000);
-
+const user = {
+    firstName: 'Alex',
+    lastName: 'Doe',
+    age: 25,
+    telNum: '+380432234234',
+    footSize: undefined,
+    superId: Symbol(),
+    address: null,
+    sayHello: function(){
+        return `${this.firstName} say: Hello!`
+    }
 }
 
-const btn = document.querySelector('button');
-btn.addEventListener('click', function() {
-    debugger;
-   clearTimeout(id);
-})
+const str = JSON.stringify(user);
+
+JSON.parse(str); 
+
+/*
+address: null
+age:25
+firstName:"Alex"
+lastName:"Doe"
+telNum:"+380432234234"
+*/
