a8ec12cfc291a0bb8355ca94db96180acfbe75ba
Mykhailenko Mykhailo <mikhailenkomikhail1995@gmail.com>
2022-11-02 20:23:04 +0200

    Task resolve

--- index.js
+++ index.js
@@ -5,8 +5,8 @@ Resolve - вирішений (успішний)
 Reject - відхилений (неуспішний)
 */
 
-const resultOfRequest = fetch('./data.json');
-console.log(resultOfRequest);
+// const resultOfRequest = fetch('./data.json');
+// console.log(resultOfRequest);
 
 // resultOfRequest.then((responce)=>{
 //     console.log('promise ok');
@@ -20,13 +20,35 @@ console.log(resultOfRequest);
 // })
 
 
-resultOfRequest
-.then((responce)=>{
-    console.log('promise ok');
-    return responce.json();
+// resultOfRequest
+// .then((responce)=>{
+//     console.log('promise ok');
+//     return responce.json();
+// }).then((successData) => {
+//     console.log(successData);
+// })
+// .catch((reject)=>{
+//     console.log(reject);
+// })
+
+
+/*
+1. Встановити LiveServer
+2. Створити json-файл
+3. Зробити запит на json (піднявши index.html -> LiveServer)
+4. Прийняти відповідь, розпарсити її як json та вивести на консоль
+*/
+
+const result = fetch('./data.json');
+console.log(result)
+
+result
+.then((responce) => {
+    console.log('promise ok')  
+    return responce.json()  
 }).then((successData) => {
-    console.log(successData);
+    console.log(successData)
 })
-.catch((reject)=>{
-    console.log(reject);
+.catch ((reject) => {
+    console.log(reject)
 })
