b71d0efb4cf87201b945f841e16fdf6659f0fd1f
Mykhailenko Mykhailo <mikhailenkomikhail1995@gmail.com>
2022-11-02 16:44:48 +0200

    Recursive Timeout

--- index.js
+++ index.js
@@ -1,13 +1,16 @@
 const h1 = document.querySelector('h1');
 
-const id = setInterval(tick, 1000);
+let id = setTimeout(tick, 1000);
 let counter = 0;
 
 function tick(){
     h1.textContent = ++counter;
+    id = setTimeout(tick, 1000);
+
 }
 
 const btn = document.querySelector('button');
 btn.addEventListener('click', function() {
-    clearInterval(id);
+    debugger;
+   clearTimeout(id);
 })
