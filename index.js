fadd23881f1f7d7f0ee5ec43a64bc38499b7714e
Mykhailenko Mykhailo <mikhailenkomikhail1995@gmail.com>
2022-11-04 13:30:02 +0200

    Spinner add

--- index.js
+++ index.js
@@ -1,15 +1,27 @@
 const API_BASE = 'https://dummyjson.com/products';
 const root = document.querySelector('#root');
 
+const spinner = document.createElement('div');
+spinner.classList.add('loader');
+/* <div class="loader">Loading...</div> */
+
+root.append(spinner);
+
 fetch(API_BASE)
 .then((responce)=>{
-   return responce.json();
+   return responce.json()
 })
 .then((data)=>{
    const liArray = data.products.map(createCard);
    const ul = document.createElement('ul');
    ul.append(...liArray);
    root.append(ul);
+})
+.catch((error)=>{
+    root.append('Some error happening')
+})
+.finally(()=>{
+    spinner.remove();
 })
 
 
