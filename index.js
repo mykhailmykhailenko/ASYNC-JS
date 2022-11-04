a69132d701233cc1f10b5dd1abe7183d9bf24134
Mykhailenko Mykhailo <mikhailenkomikhail1995@gmail.com>
2022-11-04 12:54:28 +0200

    Whos first?

--- index.js
+++ index.js
@@ -1,45 +1,16 @@
-/// Promise
-/*
-Створення промісу
-const promise = new Promise(executor);
-executor = function
-*/
-
-
-// const promise = new Promise(function(resolve, reject){
-//     /* if(it`s okay){
-//          resolve()
-//      } else {
-//          reject()
-//      }
-//      */
-// });
-
-// ///////////////////
-
-// promise.then(()=>{
-//  ///resolve callback
-// },()=>{
-//  //reject callback
-// })
-
-
-/*
-Створіть новий проміс, який буде викликати resolve, якщо 2+2 === 4
-і reject,якщо ні.
-Навісити обробники через then, які виведуть на консоль результат
-*/
-
-const promise = new Promise (function (resolve, reject) {
-    if ((2+2) === 4) {
-        resolve('TRUE')
-    }else {
-        reject('FALSE')
-    }
+const promise = new Promise(function(resolve, reject){
+    resolve('Promise RESOLVE');
 });
 
-promise.then((string)=>{
-    console.log ('Resolve:', string)
-},()=>{
-    console.log ('Reject:', error)
-});
+setTimeout(function(){
+    console.log('tick')
+},0);
+
+promise.then((string) => {
+    console.log(string);
+    return Promise.resolve('New Promise resolve')
+}, (error) => {
+    console.log('REJECT: ', error);
+}).then((value) => {
+    console.log(value);
+})
