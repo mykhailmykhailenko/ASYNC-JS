1dfcb948a18bf19cf608441e638d5d08144182c5
Mykhailenko Mykhailo <mikhailenkomikhail1995@gmail.com>
2022-11-04 10:25:20 +0200

    Creating promise

--- index.js
+++ index.js
@@ -1,35 +1,24 @@
 /// Promise
 /*
-Pending - очікування
-Resolve - вирішений (успішний)
-Reject - відхилений (неуспішний)
+Створення промісу
+const promise = new Promise(executor);
+executor = function
 */
 
-const resultOfRequest = fetch('./data.json');
 
-resultOfRequest
-.then((responce)=>responce.json())
-.then((successData) => {
-    const card = createUserCard(successData);
-    const root = document.querySelector('#root');
-    root.append(card);
-})
-.catch((reject)=>{
-    console.log(reject);
+const promise = new Promise(function(resolve, reject){
+    /* if(it`s okay){
+         resolve()
+     } else {
+         reject()
+     }
+     */
 });
 
-function createUserCard(user) {
-    const h2 = createElement('h2', {classNames: ['username']}, user.firstName, user.lastName);
-    const p = createElement('p', {}, user.telNum);
-    const div = createElement('div', {classNames: ['card-wrapper']}, h2, p);
-    return div;
-}
+///////////////////
 
-
-
-function createElement(type, {classNames=[]}, ...children) {
-    const elem = document.createElement(type);
-    elem.classList.add(...classNames);
-    elem.append(...children);
-    return elem;
-}
+promise.then(()=>{
+ ///resolve callback
+},()=>{
+ //reject callback
+})
