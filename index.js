104e226fc83204a76b2be3bcf90ea5aa44b4079b
Mykhailenko Mykhailo <mikhailenkomikhail1995@gmail.com>
2022-11-07 19:51:23 +0200

    Async/await

--- index.js
+++ index.js
@@ -1,32 +1,12 @@
-const API_BASE = 'https://dummyjson.com/products';
-const root = document.querySelector('#root');
+/* async/await */
 
-const spinner = document.createElement('div');
-spinner.classList.add('loader');
-/* <div class="loader">Loading...</div> */
-
-root.append(spinner);
-
-fetch(API_BASE)
-.then((responce)=>{
-   return responce.json()
-})
-.then((data)=>{
-   const liArray = data.products.map(createCard);
-   const ul = document.createElement('ul');
-   ul.append(...liArray);
-   root.append(ul);
-})
-.catch((error)=>{
-    root.append('Some error happening')
-})
-.finally(()=>{
-    spinner.remove();
-})
-
-
-function createCard(obj){
-    const li = document.createElement('li');
-    li.append(obj.title);
-    return li;
+async function loadImage() {
+    // const promise = await fetch('https://dummyjson.com/products/1');
+    const promise = await Promise.resolve(5);
+    ///// цей код буде відкладено
+    console.log(promise)
 }
+
+const result = loadImage();
+
+console.log('Sync code');
